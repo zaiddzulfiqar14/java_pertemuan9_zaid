e55bd1d781a820d08d463b307101eb4533ed532d
zaiddzulfiqar14 <zaid.dzzulfiqar@gmail.com>
2019-11-14 11:10:07 +0700

    java_pertemuan9_zaid

---
+++ Pertemuan9/src/pertemuan9/Latihan10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author 
+ * Nama : Zaid Dzulfiqar
+ * Kelas : TI-1A
+ * NIM : A2.1900190
+ */
+public class Latihan10 {
+    public static void main (String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
---
+++ Pertemuan9/src/pertemuan9/Mamalia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author 
+ * Nama : Zaid Dzulfiqar
+ * Kelas : TI-1A
+ * NIM : A2.1900190
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
